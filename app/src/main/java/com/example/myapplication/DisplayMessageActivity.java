30a119754c5c8e8f38efaaa36a0ba539242681fc
Kunywana <kunywananigyep@gmail.com>
2020-02-23 18:14:37 +0300

    Added  recycler,list views and splash activity

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -1,14 +1,20 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class DisplayMessageActivity extends AppCompatActivity implements Recycler_Adapter.ItemClickListener{
+    Recycler_Adapter messageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +22,38 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra("MESSAGE");
-        TextView messageView = (TextView)findViewById(R.id.messageTextView);
+        TextView messageView = findViewById(R.id.messageTextView);
         messageView.setText(message);
+
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("RecyclerView Messages : "+message);
+        arrayList.add("Hi ");
+        arrayList.add("What are your plans today?");
+        arrayList.add("Would you like some Ice Cream?");
+        arrayList.add("Do you have a boyfriend,");
+        arrayList.add("Dora?");
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        messageAdapter = new Recycler_Adapter(this, arrayList);
+        messageAdapter.setClickListener(this);
+        recyclerView.setAdapter(messageAdapter);
+
+
+
+
+
+
 
     }
 
     public void onClose(View view){
         super.finish();
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + messageAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/myapplication/Songs.java
+++ app/src/main/java/com/example/myapplication/Songs.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Songs extends AppCompatActivity {
 
@@ -14,5 +18,17 @@ public class Songs extends AppCompatActivity {
 
         MediaPlayer mediaPlayer = MediaPlayer.create(Songs.this,R.raw.givenchy);
         mediaPlayer.start();
+
+        ListView listView = findViewById(R.id.songs);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Intentions - Justin Beiber");
+        arrayList.add("How do you sleep - Sam Smith");
+        arrayList.add("Sore eyes - Calum Scott");
+        arrayList.add("Ocean eyes - Billie Eilish");
+        arrayList.add("Yummy - Justin Beiber");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
